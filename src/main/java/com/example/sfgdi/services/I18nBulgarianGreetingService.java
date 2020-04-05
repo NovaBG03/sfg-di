f3e6c97b6d85745be64ff31b42b62a5e7e5886f2
Nikita Koynov <nikicha03@gmail.com>
2020-04-05 17:48:16 +0300

    'profiles'

---
+++ src/main/java/com/example/sfgdi/services/I18nBulgarianGreetingService.java
@@ -0,0 +1,14 @@
+package com.example.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("BG")
+@Service("i18nGreetingService")
+public class I18nBulgarianGreetingService implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "Здравей Свят! - БГ";
+    }
+}
