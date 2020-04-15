bbdf0e5dfdf852204c735e24eccd883163fd0e6f
Nikita Koynov <nikicha03@gmail.com>
2020-04-15 12:48:42 +0300

    properties-sorce

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package com.example.sfgdi;
 
 import com.example.sfgdi.controllers.*;
+import com.example.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,24 +13,9 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-		System.out.println(i18nController.getGreeting());
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
 
-		System.out.println("----------- Primary");
-		MyController controller = (MyController) ctx.getBean("myController");
-		System.out.println(controller.sayHello());
-
-		System.out.println("---------- Property");
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("------------ Setter");
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("------- Constructor");
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.getGreeting());
+		System.out.println(fakeDataSource.getUsername());
 	}
 
 }
